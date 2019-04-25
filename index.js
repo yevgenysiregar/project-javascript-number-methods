95ef3c0beeb2f6e841fbb107c60d12d7a65483c2
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-25 20:41:05 +0700

    Add code to index.js to try using number methods

---
+++ index.js
@@ -0,0 +1,9 @@
+const age = 19;
+console.log(age.toString());
+console.log(age.toString(2));
+console.log(age.toString(16));
+console.log(age.toString(36));
+
+const number = 12.5623;
+console.log(number.toFixed(1));
+console.log(number.toFixed(3));
